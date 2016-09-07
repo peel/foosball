23e50012374b28511bd838eebbd1493968a43620
peel <plimanowski@gmail.com>
2016-09-07 18:16:07 +0200

    Use production config from Heroku ENV variables and decrease socket timeout

--- apps/rumbl/config/prod.secret.exs
+++ apps/rumbl/config/prod.secret.exs
@@ -5,11 +5,14 @@ use Mix.Config
 # your version control system.
 config :rumbl, Rumbl.Endpoint,
   secret_key_base: "RKVuHV3Pv+dSZ2GkLdlpvGxu5ZkD8YDoRNaN6yfUP+ce8zspN9yeUbZkRGHH8Jsx"
+  http: [port: {:system, "PORT"}],
+  url: [host: "secure-escarpment-62712.herokuapp.com", port: 80],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
 config :rumbl, Rumbl.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "rumbl_prod",
-  pool_size: 20
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
